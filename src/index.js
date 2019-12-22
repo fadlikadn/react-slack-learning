79ccda5b423c74666222c127e17e35392b556092
Fadlika Dita Nurjanto <fadlikadn@gmail.com>
2019-12-22 16:13:22 +0700

    onAuthStateChanged to Listen for Login/Register

--- src/index.js
+++ src/index.js
@@ -4,20 +4,37 @@ import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import registerServiceWorker from './registerServiceWorker';
+import firebase from "firebase";
 
 import 'semantic-ui-css/semantic.min.css';
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
 
-const Root = () => (
+class Root extends React.Component {
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                this.props.history.push('/');
+            }
+        });
+    }
+
+    render() {
+        return (
+            <Switch>
+                <Route exact path="/" component={App}></Route>
+                <Route path="/login" component={Login}></Route>
+                <Route path="/register" component={Register}></Route>
+            </Switch>
+        );
+    }
+}
+
+const RootWithAuth = withRouter(Root);
+
+ReactDOM.render(
     <Router>
-        <Switch>
-            <Route exact path="/" component={App}></Route>
-            <Route path="/login" component={Login}></Route>
-            <Route path="/register" component={Register}></Route>
-        </Switch>
-    </Router>
-);
-
-ReactDOM.render(<Root />, document.getElementById('root'));
+        <RootWithAuth/>
+    </Router>,
+    document.getElementById('root'));
 registerServiceWorker();
