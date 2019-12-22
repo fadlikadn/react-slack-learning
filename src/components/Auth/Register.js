894492c307e3bc183d42d9889e920accde1981dd
Fadlika Dita Nurjanto <fadlikadn@gmail.com>
2019-12-22 14:26:01 +0700

    Add name and avatar to users, add new users to database

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid, Form, Segment, Button, Header, Message, Icon } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import firebase from '../../firebase';
+import md5 from 'md5';
 
 class Register extends React.Component{
     state = {
@@ -11,6 +12,7 @@ class Register extends React.Component{
         passwordConfirmation: '',
         errors: [],
         loading: false,
+        usersRef: firebase.database().ref('users'),
     };
 
     isFormValid = () => {
@@ -61,13 +63,35 @@ class Register extends React.Component{
                 .createUserWithEmailAndPassword(this.state.email, this.state.password)
                 .then(createdUser => {
                     console.log(createdUser);
-                    this.setState({ loading: false });
+                    // this.setState({ loading: false });
+                    createdUser.user
+                        .updateProfile({
+                        displayName: this.state.username,
+                        photoURL: `http://gravatar.com/avatar/${md5(createdUser.user.email)}?d=identicon`,
+                        })
+                        .then(() => {
+                            this.setState({ loading: false });
+                            this.saveUser(createdUser).then(() => {
+                                console.log('user saved');
+                            });
+                        })
+                        .catch(err => {
+                            console.error(err);
+                            this.setState({ errors: this.state.errors.concat(err), loading: false });
+                        })
                 })
                 .catch(err => {
                     console.error(err);
                     this.setState({ errors: this.state.errors.concat(err),  loading: false });
                 });
         }
+    };
+
+    saveUser = createdUser => {
+        return this.state.usersRef.child(createdUser.user.uid).set({
+            name: createdUser.user.displayName,
+            avatar: createdUser.user.photoURL,
+        });
     };
 
     handleInputError = (errors, inputName) => {
