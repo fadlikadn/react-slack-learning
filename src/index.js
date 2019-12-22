402c85a7f5d98ede046ff68afca783491977256d
Fadlika Dita Nurjanto <fadlikadn@gmail.com>
2019-12-22 16:30:32 +0700

    set up redux to make user data available on Global State

--- src/index.js
+++ src/index.js
@@ -10,10 +10,20 @@ import 'semantic-ui-css/semantic.min.css';
 
 import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
 
+import { createStore } from 'redux';
+import { Provider, connect} from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from "./reducers";
+import { setUser} from "./actions";
+
+const store = createStore(rootReducer, composeWithDevTools());
+
 class Root extends React.Component {
     componentDidMount() {
         firebase.auth().onAuthStateChanged(user => {
             if (user) {
+                // console.log(user);
+                this.props.setUser(user);
                 this.props.history.push('/');
             }
         });
@@ -30,11 +40,13 @@ class Root extends React.Component {
     }
 }
 
-const RootWithAuth = withRouter(Root);
+const RootWithAuth = withRouter(connect(null, { setUser })(Root));
 
 ReactDOM.render(
-    <Router>
-        <RootWithAuth/>
-    </Router>,
+    <Provider store={store}>
+        <Router>
+            <RootWithAuth/>
+        </Router>
+    </Provider>,
     document.getElementById('root'));
 registerServiceWorker();
