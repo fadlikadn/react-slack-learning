d09426c94d402671b512c8d3c921428de59fed35
Fadlika Dita Nurjanto <fadlikadn@gmail.com>
2019-12-26 17:18:50 +0700

    Display Messages, Create Message Component

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,18 +1,55 @@
 import React from 'react';
 import MessagesHeader from "./MessagesHeader";
 import MessageForm from "./MessageForm";
+import Message from "./Message";
 import { Segment, Comment } from 'semantic-ui-react';
 import firebase from '../../firebase';
 
 class Messages extends React.Component {
     state = {
         messagesRef: firebase.database().ref('messages'),
+        messages: [],
+        messagesLoading: true,
         channel: this.props.currentChannel,
         user: this.props.currentUser,
     };
 
+    componentDidMount() {
+        const { channel, user } = this.state;
+
+        if (channel && user) {
+            this.addListeners(channel.id);
+        }
+    };
+
+    addListeners = channelId => {
+        this.addMessageListener(channelId);
+    };
+
+    addMessageListener = channelId => {
+        let loadedMessages = [];
+        this.state.messagesRef.child(channelId).on('child_added', snap => {
+            loadedMessages.push(snap.val());
+            console.log(loadedMessages);
+            this.setState({
+                messages: loadedMessages,
+                messagesLoading: false,
+            });
+        });
+    };
+
+    displayMessages = messages => (
+        messages.length > 0 && messages.map(message => (
+            <Message
+                key={message.timestamp}
+                message={message}
+                user={this.state.user}
+            />
+        ))
+    );
+
     render() {
-        const { messagesRef, channel, user } = this.state;
+        const { messagesRef, messages, channel, user } = this.state;
 
         return (
             <React.Fragment>
@@ -21,6 +58,7 @@ class Messages extends React.Component {
                 <Segment>
                     <Comment.Group className="messages">
                         {/*Messages*/}
+                        {this.displayMessages(messages)}
                     </Comment.Group>
                 </Segment>
 
