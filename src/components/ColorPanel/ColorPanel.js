d39700ddc2173380f87bcd28f9fe5dc462894f16
Fadlika Dita Nurjanto <FadlikaDita.Nurjanto@mitrais.com>
2020-06-29 13:54:08 +0700

    Add Color Picker Modal to Color Panel

--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import { Sidebar, Menu, Divider, Button } from 'semantic-ui-react';
+import { Sidebar, Menu, Divider, Button, Modal, Icon, Label } from 'semantic-ui-react';
+import { SketchPicker } from 'react-color';
 
 class ColorPanel extends React.Component {
+    state = {
+        modal: false
+    }
+
+    openModal = () => this.setState({ modal: true });
+
+    closeModal = () => this.setState({ modal: false });
+
     render() {
+        const { modal } = this.state;
+
         return (
            <Sidebar
                 as={Menu}
@@ -13,7 +24,26 @@ class ColorPanel extends React.Component {
                 width="very thin"
             >
                 <Divider/>
-                <Button icon="add" size="small" color="blue"/>
+                <Button icon="add" size="small" color="blue" onClick={this.openModal}/>
+
+                {/* Color Picker Modal */}
+                <Modal basic open={modal} onClose={this.closeModal}>
+                    <Modal.Header>Choose App Colors</Modal.Header>
+                    <Modal.Content>
+                        <Label content="Primary Color"/>
+                        <SketchPicker/>
+                        <Label content="Secondary Color"/>
+                        <SketchPicker/>
+                    </Modal.Content>
+                    <Modal.Actions>
+                        <Button colors="green" inverted>
+                            <Icon name="checkmark"/> Save Colors
+                        </Button>
+                        <Button colors="red" inverted onClick={this.closeModal}>
+                            <Icon name="remove"/> Cancel
+                        </Button>
+                    </Modal.Actions>
+                </Modal>
 
             </Sidebar>
         )
